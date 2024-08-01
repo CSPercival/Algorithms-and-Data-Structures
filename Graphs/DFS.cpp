81ce987dd9f58725c4db9d9329af1f4acb8957d7
Cyryl Szatan <cszatan@LAPTOP-OMEN>
2024-08-01 22:42:08 +0200

    add DFS algorithm

---
+++ Graphs/DFS.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct DFS{
+    int n;                  // n - number of vertices
+    vector<vector<int>> G;  // adjacency list
+    vector<int> vis;        // visited array
+    DFS(int in_n, vector<vector<int>>& in_G){
+        n = in_n;
+        G = in_G;
+        vis.resize(n);
+    }
+
+    void traverse(int v){
+        vis[v] = 1;
+        for(int u : G[v]){
+            if(vis[u] == 1) continue;
+            traverse(u);
+        }
+    }
+};
