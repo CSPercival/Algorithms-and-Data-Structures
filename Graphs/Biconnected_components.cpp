c86486e737f76f937f39fe8dfbfd1ddbf837f85d
Cyryl Szatan <cszatan@LAPTOP-OMEN>
2024-08-29 00:11:05 +0200

    replace ArticulationPoints structure to Low because it is simpler

--- Graphs/Biconnected_components.cpp
+++ Graphs/Biconnected_components.cpp
@@ -10,36 +10,31 @@ template <typename Other> static inline tag <ostream> operator<<(tag <ostream> o
 template <typename T> static inline tag <ostream> operator <<(tag <ostream> os, vector <T> const& v){ os.get()<<"["; for (int i=0; i<v.size(); i++) if (i!=v.size()-1) os.get()<<v[i]<<", "; else os.get()<<v[i]; return os.get()<<"]", os; }
 template <typename T> static inline tag <ostream> operator <<(tag <ostream> os, set <T> const& s){ vector <T> v; for (auto i: s) v.push_back(i); os.get()<<"["; for (int i=0; i<v.size(); i++) if (i!=v.size()-1) os.get()<<v[i]<<", "; else os.get()<<v[i]; return os.get()<<"]", os; }
 
-
-struct ArticulationPoints{
-    vector<vector<int>> *graph;
-    vector<int> low,vis;
-    vector<int> *ans;
+struct Low{
+    vector<vector<int>>* graph;
+    vector<int> *low;
+    vector<int> vis;
+    int n;
     int preorder = 0;
 
-    ArticulationPoints(int in_n, vector<vector<int>> *in_graph, vector<int> *in_ans){
-        graph = in_graph; ans = in_ans;
-        vis.assign(in_n + 1,0); low.assign(in_n + 1,0);
+    Low(int in_n, vector<vector<int>>* in_graph, vector<int> *ans){
+        n = in_n; graph = in_graph; low = ans;
+        vis.assign(n + 1,0);
+        low->assign(n + 1,0);
         dfs(1,0);
     }
 
     void dfs(int idx, int par){
-        low[idx] = ++preorder;
+        (*low)[idx] = ++preorder;
         vis[idx] = preorder;
-        int children = 0;
-        int max_children_low = 0;
         for(int i : (*graph)[idx]){
             if(i == par) continue;
-            if(vis[i]) low[idx] = min(low[idx], vis[i]);
+            if(vis[i]) (*low)[idx] = min((*low)[idx], vis[i]);
             else {
-                children++;
                 dfs(i,idx);
-                low[idx] = min(low[idx],low[i]);
-                max_children_low = max(max_children_low, low[i]);
+                (*low)[idx] = min((*low)[idx],(*low)[i]);
             }
         }
-        if(idx == 1 && children > 1) ans->push_back(idx);
-        if(idx != 1 && max_children_low >= vis[idx]) ans->push_back(idx);
     }
 };
 
@@ -48,19 +43,16 @@ struct BCC{
     vector<vector<int>> *graph;
     vector<int> bcc_stack;
     int n;
-    vector<int> vis, arti;
-    vector<int> *low;
+    vector<int> vis;
+    vector<int> low;
     vector<int> *preorder;
     int bcc_ctr = 0;
 
     BCC(int in_n, vector<vector<int>> *in_graph, vector<vector<int>> *in_ans){
         n = in_n; graph = in_graph; ans = in_ans;
-        vis.assign(n + 1, 0); arti.assign(n + 1, 0);
-        vector<int> algo_v;
-        ArticulationPoints algo(n,graph,&algo_v);
-        low = (&algo.low);
+        vis.assign(n + 1, 0);
+        Low algo(n,graph,&low);
         preorder = (&algo.vis);
-        for(int i : algo_v) arti[i] = 1;
         ans->emplace_back();
         bcc_stack.push_back(0);
         dfs(1);
@@ -71,13 +63,13 @@ struct BCC{
         (*ans)[bcc_stack.back()].push_back(idx);
         for(int i : (*graph)[idx]){
             if(vis[i]) continue;
-            if((*low)[i] >= (*preorder)[idx]){
+            if(low[i] >= (*preorder)[idx]){
                 bcc_stack.push_back(++bcc_ctr);
                 ans->emplace_back();
                 (*ans)[bcc_stack.back()].push_back(idx);
             }
             dfs(i);
-            if((*low)[i] >= (*preorder)[idx]){
+            if(low[i] >= (*preorder)[idx]){
                 bcc_stack.pop_back();
             }
         }
