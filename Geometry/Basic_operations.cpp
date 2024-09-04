ffaffa1235087347ca28a5a7f79a67349a514314
Cyryl Szatan <cszatan@LAPTOP-OMEN>
2024-09-05 00:47:43 +0200

    add Circle - Line intersection to basic geometric operation. Untested

--- Geometry/Basic_operations.cpp
+++ Geometry/Basic_operations.cpp
@@ -24,7 +24,9 @@ struct BGeo{
     pair<Point,Point> intersection(Line L, Circle C){
         Line tL= {L.A,L.B,L.C + C.x * L.A + C.y * L.B};
         Circle tC = {0,0,C.r};
-    
 
+        Point proj = {(-1 * tL.C * tL.A) / (tL.A * tL.A + tL.B * tL.B), (-1 * tL.C * tL.B) / (tL.A * tL.A + tL.B * tL.B)};
+        ld h = sqrt(tC.r * tC.r - proj.x * proj.x - proj.y * proj.y);
+        return {{proj.x + h * L.A, proj.y - h * L.B},{proj.x - h * L.A, proj.y + h * L.B}}
     }
 };
