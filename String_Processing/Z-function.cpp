02ab6cd855ce14a94dd37b104c8ba91651504fae
CSPercival <cyrek2003@gmail.com>
2024-12-23 23:59:04 +0100

    add Z-function to String_Processing section

---
+++ String_Processing/Z-function.cpp
@@ -0,0 +1,35 @@
+/*
+#############################################
+#                                           #
+#           Quality Certificate             #
+#                I class                    #
+#                                           #
+#   Tasks:                                  #
+#       - String Matching                   #
+#           (CSES Problem Set)              #
+#                                           #
+#############################################
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void ZFunction(string &s, vector<int> *z){
+    (*z).assign(s.size(), 0);
+    int rend = 0, lend = 0;
+    int shift;
+    for(int i = 1; i < (int)s.size(); i++){
+        shift = 0;
+        if(i < rend){
+            shift = min(rend - i,(*z)[i - lend]);
+        }
+        while(i + shift >= rend && s[shift] == s[i + shift]) shift++;
+
+        (*z)[i] = shift;
+        if(shift != 0) shift--;
+        if(i + shift > rend){
+            rend = i + shift;
+            lend = i;
+        }
+    }
+}
